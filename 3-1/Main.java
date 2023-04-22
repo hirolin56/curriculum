030aaee6b652c701f043602ecd2fc0b579134860
Lin　Yingda <51394591+hirolin56@users.noreply.github.com>
2023-04-22 19:13:15 +0900

    第3章

---
+++ 3-1/Main.java
@@ -0,0 +1,5 @@
+package 3-1;
+
+public class Main {
+    
+}
---
+++ JavaStudy/forJava/FizzBuzz.java
@@ -0,0 +1,29 @@
+package JavaStudy.forJava;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            //3と5の両方で割り切れる時はFizzBuzzと表示
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+
+            //3と5の両方で割り切れる時はFizzBuzzを表示
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            //3で割り切れる時はFizzを表示
+            else if(i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            //5で割り切れる時はBuzzを表示
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            //それ以外の時は変数 i を表示
+            else {
+                System.out.println(i);
+            }
+            }
+        }
+    }
+}
