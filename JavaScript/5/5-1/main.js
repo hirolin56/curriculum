ba34fecac88c4c7c95ec13743cbbade43d0b6d53
Lin　Yingda <51394591+hirolin56@users.noreply.github.com>
2023-07-25 22:09:12 +0900

    Javascript 課題5-4

---
+++ JavaScript/5/5-1/main.js
@@ -0,0 +1,18 @@
+// document.querySelector('#createBtn').addEventListener('click', function(){
+//     let item = document.createElement('li');
+//     item.textContent = 'item';
+
+//     let ul = document.querySelector('ul');
+//     ul.appendChild(item);
+// })
+
+
+
+document.querySelector('#createBtn').addEventListener('click', function() {
+    let item = document.createElement('li');
+    item.textContent = 'item';
+
+    let ul = document.querySelector('ul');
+    ul.appendChild(item);
+    console.log(item);
+});
